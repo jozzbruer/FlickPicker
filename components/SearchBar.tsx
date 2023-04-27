8eff73c003e0580e0465439ce04e89b08a9cd8a4
Joz-Bruer Quince <quincejozbruer@gmail.com>
2023-04-27 00:47:05 -0700

    Setup data fetching

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,28 +1,47 @@
+import { AppContext } from '@/context/AppContextProvider';
 import { SearchIcon } from '@chakra-ui/icons';
 import {
+	Box,
 	Flex,
 	FormControl,
 	Input,
 	InputGroup,
 	InputLeftElement,
 } from '@chakra-ui/react';
+import { useContext, useState } from 'react';
 
 const SearchBar: React.FC = () => {
+	const { fetchData, movies } = useContext(AppContext);
+	const [searchValue, setSearchValue] = useState('');
+
+	const searchHandler = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		fetchData(searchValue);
+	};
+
+	console.log(movies);
 	return (
 		<Flex
 			alignItems='center'
 			justifyContent='center'
 			width='100vw'
 			height='30vh'>
-			<FormControl width='70%'>
-				<InputGroup>
-					<InputLeftElement
-						pointerEvents='none'
-						children={<SearchIcon color='gray.300' h='6' w='6' />}
-					/>
-					<Input size='lg' placeholder='Search...' />
-				</InputGroup>
-			</FormControl>
+			<Box width='70%'>
+				<form onSubmit={searchHandler}>
+					<InputGroup>
+						<InputLeftElement
+							pointerEvents='none'
+							children={<SearchIcon color='gray.300' h='6' w='6' />}
+						/>
+						<Input
+							size='lg'
+							placeholder='Search...'
+							value={searchValue}
+							onChange={(e) => setSearchValue(e.target.value)}
+						/>
+					</InputGroup>
+				</form>
+			</Box>
 		</Flex>
 	);
 };
--- context/AppContextProvider.tsx
+++ context/AppContextProvider.tsx
@@ -1,11 +1,55 @@
-import { ReactNode, createContext } from 'react';
-export const AppContext = createContext('');
+import axios, { AxiosResponse } from 'axios';
+import React, { ReactNode, createContext, useState } from 'react';
+
+interface IMovieContext {
+	movies: any[];
+	loading: boolean;
+	error: string | null;
+	fetchData: (search: string) => void;
+}
+
+const MovieContext: IMovieContext = {
+	movies: [],
+	loading: false,
+	error: null,
+	fetchData: () => {},
+};
+export const AppContext = createContext(MovieContext);
 
 type Props = {
 	children: ReactNode;
 };
-const AppContextProvider = ({ children }: Props) => {
-	return <AppContext.Provider value=''>{children}</AppContext.Provider>;
+const AppContextProvider: React.FC<Props> = ({ children }) => {
+	const [movies, setMovies] = useState<any[]>([]);
+	const [loading, setLoading] = useState<boolean>(false);
+	const [error, setError] = useState<string | null>(null);
+	const options = {
+		headers: {
+			'X-RapidAPI-Key': process.env.NEXT_PUBLIC_API_KEY,
+			'X-RapidAPI-Host': process.env.NEXT_PUBLIC_HOST,
+		},
+	};
+
+	const fetchData = async (searchText: string) => {
+		setLoading(true);
+		try {
+			const response: AxiosResponse = await axios.get(
+				`https://movie-database-alternative.p.rapidapi.com/?s=${searchText}`,
+				options
+			);
+			const responseData = await response.data.Search;
+			setMovies(responseData);
+			setLoading(false);
+		} catch (error: any) {
+			setError(error.message);
+			setLoading(false);
+		}
+	};
+	return (
+		<AppContext.Provider value={{ movies, loading, error, fetchData }}>
+			{children}
+		</AppContext.Provider>
+	);
 };
 
 export default AppContextProvider;
